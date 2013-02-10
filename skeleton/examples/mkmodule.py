f74538f9b6ec637cce3ca8aba380e3fbacba3b4a
mathtick <davidcottrell@notsohard.local>
2013-02-09 23:39:18 -0500

    Make it possible to simple link to mkmodule.py and use the default templates in examples.

--- skeleton/examples/mkmodule.py
+++ skeleton/examples/mkmodule.py
@@ -1,15 +1,19 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """
 Basic script to create an empty python package containing one module
 """
 from skeleton import Skeleton, Var
 
+import os
+myname = os.path.realpath(__file__)
+mypath = os.path.dirname(myname)
+myname = os.path.basename(myname)
 
 class BasicModule(Skeleton):
     """
     Create an empty module with its etup script and a README file.
     """
-    src = 'basic-module'
+    src = os.path.join(mypath, 'basic-module')
     variables = [
         Var('module_name'),
         Var('author'),
